4e5d422805d384e76f7bfe8d6b6017fa6b1aafdb
Gabriel <Gabriel.n.adeau@outlook.com>
2022-10-06 15:34:21 -0400

    tourner

--- src/main.ino
+++ src/main.ino
@@ -11,7 +11,7 @@ Inclure les librairies de functions que vous voulez utiliser
 **************************************************************************** */
 
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
-
+#include <math.h>
 
 
 /* ****************************************************************************
@@ -25,9 +25,29 @@ Variables globales et defines
 /* ****************************************************************************
 Vos propres fonctions sont creees ici
 **************************************************************************** */
-void maFonction(){
-  // code
-}
+void Tourner(int degree)
+{
+  double nbPulses = 0;
+  //Serial.println(nbPulses);
+  //bPulses = (910 * (asin((degree*(M_PI / 180.0))/2))/(M_PI / 180.0)); 
+  //Serial.println(nbPulses);
+  nbPulses = 3840; //1 roue
+  nbPulses = 3700; //2 roues
+
+  nbPulses = (3840/90)*degree;
+  int i = 0;
+
+  while (i <= nbPulses)
+  {
+    Serial.println(nbPulses);
+    MOTOR_SetSpeed(0, 0.3);
+    //MOTOR_SetSpeed(1, -0.3);
+    i += ENCODER_ReadReset(0);
+  }
+  
+  MOTOR_SetSpeed(0, 0);
+  //MOTOR_SetSpeed(1, 0);
+} 
 
 
 /* ****************************************************************************
@@ -37,7 +57,8 @@ Fonctions d'initialisation (setup)
 // -> Se fait appeler seulement un fois
 // -> Generalement on y initilise les varibbles globales
 
-void setup(){
+void setup()
+{
   BoardInit();
 }
 
@@ -47,9 +68,14 @@ Fonctions de boucle infini (loop())
 **************************************************************************** */
 // -> Se fait appeler perpetuellement suite au "setup"
 
-void loop() {
+void loop() 
+{
   // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
   delay(10);// Delais pour décharger le CPU
   //Test pour commit via VS Code
   //test
+  Serial.println("test");
+  //Tourner(90);
+  delay(2000);
+  Tourner(45);
 }
