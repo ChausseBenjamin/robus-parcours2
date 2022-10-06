e164fee32b75a42643152d1d3532febcc8dcb644
Simon Gagne <gags2431@usherbrooke.ca>
2022-10-06 15:13:31 -0400

    PID V1

--- src/main.ino
+++ src/main.ino
@@ -1,17 +1,15 @@
 /*
-Projet: Le nom du script
-Equipe: Votre numero d'equipe
-Auteurs: Les membres auteurs du script
-Description: Breve description du script
+Projet: DefiDuParcous2
+Equipe: P6
+Auteurs: Simon Gagné
+Description: Programme pour du robot #2 pour le défi du parcours
 Date: Derniere date de modification
 */
 
 /* ****************************************************************************
 Inclure les librairies de functions que vous voulez utiliser
 **************************************************************************** */
-
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
-
 
 
 /* ****************************************************************************
@@ -21,12 +19,76 @@ Variables globales et defines
 // L'ensemble des fonctions y ont acces
 
 
-
 /* ****************************************************************************
 Vos propres fonctions sont creees ici
 **************************************************************************** */
-void maFonction(){
-  // code
+
+//Avance le robot sur une distance
+void Avancer(float distance)
+{
+	// code
+	int PULSE_PAR_CM = 3200 / (7.62*PI); //3pouces de diametres = 7,62 cm
+
+	int pulseAFaire = distance * PULSE_PAR_CM;
+	int pulseFait = 0;
+	int nbVerif = 0;
+	int nbPulseVouluParIntervalle = 500;
+	int intervalleTemps = 100;
+
+	int pulseReelTotalGauche = 0;
+	int pulseReelTotalDroit = 0;
+
+	//To be updated
+	float kp = 0.0001;
+	float ki = 0.00002;
+
+	float previousSpeedGauche = 0.3;
+	float previousSpeedDroit = 0.3;
+	MOTOR_SetSpeed(LEFT, previousSpeedGauche);
+	MOTOR_SetSpeed(RIGHT, previousSpeedDroit);
+
+	while (pulseFait < pulseAFaire)
+	{
+		delay(intervalleTemps);
+		nbVerif++;
+		int nbPulseReelGauche = ENCODER_Read(LEFT);
+		int nbPulseReelDroit = ENCODER_Read(RIGHT);
+		int erreurGauche = nbPulseVouluParIntervalle - nbPulseReelGauche;
+		int erreurDroit = nbPulseVouluParIntervalle - nbPulseReelDroit;
+
+		Serial.print("Nb pusle reel : ");
+		Serial.print(nbPulseReelDroit);
+		Serial.print(" ----- Motor Speed Left: ");
+		Serial.print(previousSpeedGauche);
+		Serial.print(" ----- Erreur Gauche: ");
+		Serial.print(erreurGauche);
+		Serial.print("\n");
+
+		int pulseVouluTotal = nbPulseVouluParIntervalle * nbVerif;
+		pulseReelTotalGauche += nbPulseReelGauche;
+		pulseReelTotalDroit += nbPulseReelDroit;
+		ENCODER_Reset(LEFT);
+		ENCODER_Reset(RIGHT);
+		int erreurTotalGauche = pulseVouluTotal - pulseReelTotalGauche;
+		int erreurTotalDroit = pulseVouluTotal - pulseReelTotalGauche;
+
+		float correctionGauche = erreurGauche * kp + erreurTotalGauche * ki;
+		float correctionDroit = erreurDroit * kp + erreurTotalDroit * ki;
+		
+		float newSpeedGauche = previousSpeedGauche + correctionGauche;
+		float newSpeedDroit = previousSpeedDroit + correctionDroit;
+		MOTOR_SetSpeed(LEFT, newSpeedGauche);
+		MOTOR_SetSpeed(RIGHT, newSpeedDroit);
+
+		previousSpeedGauche = newSpeedGauche;
+		previousSpeedDroit = newSpeedDroit;
+		pulseFait += nbPulseReelDroit;
+	}
+}
+
+void Tourner(int moteurId, int degres)
+{
+
 }
 
 
@@ -37,8 +99,10 @@ Fonctions d'initialisation (setup)
 // -> Se fait appeler seulement un fois
 // -> Generalement on y initilise les varibbles globales
 
-void setup(){
-  BoardInit();
+void setup()
+{
+	BoardInit();
+	
 }
 
 
@@ -47,10 +111,23 @@ Fonctions de boucle infini (loop())
 **************************************************************************** */
 // -> Se fait appeler perpetuellement suite au "setup"
 
-void loop() {
-  // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
-  delay(10);// Delais pour décharger le CPU
-  //Test pour commit via VS Code
-  //test
-  //test ann-sosdfsdf 2222222
+void loop()
+{
+	// SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
+	delay(100);// Delais pour décharger le CPU
+	if (ROBUS_IsBumper(REAR))
+	{
+		//Différentes parties du parcours
+		Avancer(200);
+		/*Tourner(LEFT, 90);
+		Avancer(40);
+		Tourner(RIGHT, 45);
+		Avancer(50);*/
+		//etc
+		while (true)
+		{
+			/* do nothing --- needed to stop "loop" */
+		}
+	}
+	
 }
