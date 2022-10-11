9d1f195c43b629dca169fc4e988268624277988b
Simon Gagne <gags2431@usherbrooke.ca>
2022-10-11 12:17:07 -0400

    Sans le 180

--- src/main.ino
+++ src/main.ino
@@ -11,38 +11,36 @@ Inclure les librairies de functions que vous voulez utiliser
 **************************************************************************** */
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
 
-
 /* ****************************************************************************
 Variables globales et defines
 **************************************************************************** */
 // -> defines...
 // L'ensemble des fonctions y ont acces
 
-
 /* ****************************************************************************
 Vos propres fonctions sont creees ici
 **************************************************************************** */
 
-//Avance le robot sur une distance
+// Avance le robot sur une distance
 void Avancer(float distance)
 {
 	// code
-	int PULSE_PAR_CM = 3200 / (7.62*PI); //3pouces de diametres = 7,62 cm
+	int PULSE_PAR_CM = 3200 / (7.62 * PI); // 3pouces de diametres = 7,62 cm
 
 	int pulseAFaire = distance * PULSE_PAR_CM;
 	int pulseFait = 0;
 	int nbVerif = 0;
-	int nbPulseVouluParIntervalle = 500;
+	int nbPulseVouluParIntervalle = 400;
 	int intervalleTemps = 100;
 
 	int pulseReelTotalGauche = 0;
 	int pulseReelTotalDroit = 0;
 
-	//To be updated
-	float kp = 0.0004;
+	// To be updated
+	float kp = 0.0005;
 	float ki = 0.00008;
 
-	float previousSpeedGauche = 0.3;
+	float previousSpeedGauche = 0.32;
 	float previousSpeedDroit = 0.3;
 	MOTOR_SetSpeed(LEFT, previousSpeedGauche);
 	MOTOR_SetSpeed(RIGHT, previousSpeedDroit);
@@ -74,7 +72,7 @@ void Avancer(float distance)
 
 		float correctionGauche = erreurGauche * kp + erreurTotalGauche * ki;
 		float correctionDroit = erreurDroit * kp + erreurTotalDroit * ki;
-		
+
 		float newSpeedGauche = previousSpeedGauche + correctionGauche;
 		float newSpeedDroit = previousSpeedDroit + correctionDroit;
 		MOTOR_SetSpeed(LEFT, newSpeedGauche);
@@ -85,39 +83,38 @@ void Avancer(float distance)
 		pulseFait += nbPulseReelDroit;
 	}
 
-  MOTOR_SetSpeed(LEFT, 0);
-  MOTOR_SetSpeed(RIGHT, 0);
+	MOTOR_SetSpeed(LEFT, 0);
+	MOTOR_SetSpeed(RIGHT, 0);
 }
 
 void Tourner(int degree, int cote)
 {
-  int moteur = LEFT;
-  if (cote == LEFT)
-  {
-    moteur = RIGHT;
-  }
-  double nbPulses = 0;
-  //Serial.println(nbPulses);
-  //bPulses = (910 * (asin((degree*(M_PI / 180.0))/2))/(M_PI / 180.0)); 
-  //Serial.println(nbPulses);
-  nbPulses = 3840; //1 roue
-  nbPulses = 3700; //2 roues
+	int moteur = LEFT;
+	if (cote == LEFT)
+	{
+		moteur = RIGHT;
+	}
+	double nbPulses = 0;
+	// Serial.println(nbPulses);
+	// bPulses = (910 * (asin((degree*(M_PI / 180.0))/2))/(M_PI / 180.0));
+	// Serial.println(nbPulses);
+	nbPulses = 3840; // 1 roue
+	nbPulses = 3700; // 2 roues
 
-  nbPulses = (3840/90)*degree;
-  int i = 0;
+	nbPulses = (3840 / 90) * degree;
+	int i = 0;
 
-  while (i <= nbPulses)
-  {
-    Serial.println(nbPulses);
-    MOTOR_SetSpeed(moteur, 0.3);
-    //MOTOR_SetSpeed(1, -0.3);
-    i += ENCODER_ReadReset(moteur);
-  }
-  
-  MOTOR_SetSpeed(moteur, 0);
-  //MOTOR_SetSpeed(1, 0);
-} 
+	while (i <= nbPulses)
+	{
+		Serial.println(nbPulses);
+		MOTOR_SetSpeed(moteur, 0.3);
+		// MOTOR_SetSpeed(1, -0.3);
+		i += ENCODER_ReadReset(moteur);
+	}
 
+	MOTOR_SetSpeed(moteur, 0);
+	// MOTOR_SetSpeed(1, 0);
+}
 
 /* ****************************************************************************
 Fonctions d'initialisation (setup)
@@ -129,9 +126,7 @@ Fonctions d'initialisation (setup)
 void setup()
 {
 	BoardInit();
-	
 }
-
 
 /* ****************************************************************************
 Fonctions de boucle infini (loop())
@@ -141,29 +136,36 @@ Fonctions de boucle infini (loop())
 void loop()
 {
 	// SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
-	delay(100);// Delais pour décharger le CPU
+	delay(100); // Delais pour décharger le CPU
 	if (ROBUS_IsBumper(REAR))
 	{
-		//Différentes parties du parcours
-		Avancer(109);
+		// Différentes parties du parcours
+		/*Avancer(113);
 		Tourner(90, LEFT);
-    Avancer(90);
-    Tourner(90, RIGHT);
-    Avancer(70);
-    Tourner(45, RIGHT);
-    Avancer(175);
-    Tourner(90, LEFT);
-    Avancer(50);
-    Tourner(45, RIGHT);
-    Avancer(100);
-		//Avancer(40);
-		//Tourner(RIGHT, 45);
-		//Avancer(50);
-		//etc
+		Avancer(67.5);
+		Tourner(90, RIGHT);
+		Avancer(70);
+		Tourner(45, RIGHT);
+		Avancer(175);
+		Tourner(90, LEFT);
+		Avancer(50);
+		Tourner(45, RIGHT);
+		Avancer(100);*/
+
+		Avancer(111);
+		Tourner(90, LEFT);
+		Avancer(66);
+		Tourner(90, RIGHT);
+		Avancer(88);
+		Tourner(45, RIGHT);
+		Avancer(147);
+		Tourner(90, LEFT);
+		Avancer(39);
+		Tourner(45, RIGHT);
+		Avancer(92);
 		while (true)
 		{
 			/* do nothing --- needed to stop "loop" */
 		}
 	}
-	
 }
